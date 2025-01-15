a7454bd90adbc7915a45249d5f9336e4fcadb3a2
Jonas Raneryd Imaizumi <jonas@mediaflow.com>
2025-01-15 22:38:01 +0100

    Fixed navigation

--- fourAM/Views/ArtistsView.swift
+++ fourAM/Views/ArtistsView.swift
@@ -11,14 +11,18 @@ struct ArtistsView: View {
     @ObservedObject var libraryViewModel: LibraryViewModel
 
     var body: some View {
-        List(libraryViewModel.allArtists, id: \.self) { artist in
-            NavigationLink(destination: ArtistDetailView(
-                artist: artist,
-                albums: libraryViewModel.albums(for: artist)
-            )) {
-                Text(artist)
+        NavigationStack {
+            List(libraryViewModel.allArtists, id: \.self) { artist in
+                NavigationLink(
+                    destination: ArtistDetailView(
+                        artist: artist,
+                        albums: libraryViewModel.albums(for: artist)
+                    )
+                ) {
+                    Text(artist)
+                }
             }
+            .navigationTitle("Artists")
         }
-        .navigationTitle("Artists")
     }
 }
